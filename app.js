6360862cf3468e4253c5b4ef259e2061af6db07d
kimjl <jamesleekim90@gmail.com>
2019-05-07 18:13:32 -0400

    Added mongoose connection and body parser

--- app.js
+++ app.js
@@ -3,9 +3,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const product = require('./routes/product.route'); // Imports routes from products
-const app = express(); // initialize our express app
+// Imports routes from products
+const product = require('./routes/product.route');
+// initialize our express app
+const app = express();
 
+// Set up our mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://someuser:abcd1234@ds151876.mlab.com:51876/productstutorial';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Parse incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: false}));
 app.use('/products', product);
 
 let port = 1234;
